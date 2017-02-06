8771d6f0347b61ca32c00b11be85d6c4c61e6c20
Kristin Yetter <kayetter@gmail.com>
2017-02-06 10:22:38 -0700

    added gulpfile.js
    linked folder to lobal install of gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+gulp.task('log', function() {
+    gutil.log('workflows are awesome');
+    
+});
+
+gulp.task('default', ['log']);
+          
