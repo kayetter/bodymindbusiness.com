7c31dd62bc7c3dc596b4fcbd9f349064dd2b585b
Kristin Yetter <kayetter@gmail.com>
2017-02-06 13:07:48 -0700

    concatenate js files
    created gulp task to concatenate js files

---
+++ components/coffee/tagline.coffee
@@ -0,0 +1,8 @@
+var fill;
+
+(fill = function(item){
+    return $('.tagline').append("" + item); 
+    })('The most creative minds in Art');
+
+
+fill;
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-gulp.task('log', function() {
-    gutil.log('workflows are awesome');
+var jsSources = [
+    'components/scripts/functions.js',
+    'components/scripts/calls.js'
+]
+
+gulp.task('coffee', function() {
     
+    //grabs information in source file
+    gulp.src('components/coffee/tagline.coffee')
+             //runs it through the variable, bare = true removes safety wrapper
+             .pipe(coffee({bare:true})
+    //make sure a coffee script error doesn derail ll of gulp process
+            .on('error', gutil.log('there was a coffee error'))
+    //pipe results from coffe script and put the file in a new destination
+            .pipe(gulp.dest('components/scripts'))
+    )
+    
+});
+
+gulp.task('jsConcat', function(){
+    gulp.src(jsSources)
+    .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('builds/development/js'))
 });
 
 gulp.task('default', ['log']);
