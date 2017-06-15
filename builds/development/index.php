98f35a8654296e6daf0e39d18a5892b0d2a72cdc
Kristin Yetter <kristin@dameranchdesigns.com>
2017-06-15 14:37:37 -0600

    website opens to new tab

--- builds/development/index.php
+++ builds/development/index.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php require "head.php";  ?>
-
+<?php include "head.php";  ?>
 <body id="first-page">
-    <?php require "header.php"; ?>
+    <?php include "header.php"; ?>
         <main class="front-page">
-            <article id="hero">
-                <section class="enter">
-                    <h2>Open the door to experiencing yourself and your team in a new way. <br></h2>
-                    <a href="portal.php" class="door" alt="open portal icon"> </a>
-                </section>
-            </article>
+
         </main>
 
         <?php require "footer.php"; ?>
